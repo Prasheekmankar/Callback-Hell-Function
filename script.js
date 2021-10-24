aad721e492f8af5a769b40f4a12bacb1af957311
Prasheekmankar <89675381+Prasheekmankar@users.noreply.github.com>
2021-10-24 02:58:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let count = 10;
+const Timer = document.querySelector(".peace");
+setTimeout(() => {
+    Timer.innerHTML = `<h1>${count--}</h1>`
+    setTimeout(() => {
+        Timer.innerHTML = `<h1>${count--}</h1>`
+        setTimeout(() => {
+            Timer.innerHTML = `<h1>${count--}</h1>`
+            setTimeout(() => {
+                Timer.innerHTML = `<h1>${count--}</h1>`
+                setTimeout(() => {
+                    Timer.innerHTML = `<h1>${count--}</h1>`
+                    setTimeout(() => {
+                        Timer.innerHTML = `<h1>${count--}</h1>`
+                        setTimeout(() => {
+                            Timer.innerHTML = `<h1>${count--}</h1>`
+                            setTimeout(() => {
+                                Timer.innerHTML = `<h1>${count--}</h1>`
+                                setTimeout(() => {
+                                    Timer.innerHTML = `<h1>${count--}</h1>`
+                                    setTimeout(() => {
+                                        Timer.innerHTML = `<h1>${count--}</h1>`
+                                        setTimeout(() => {
+                                            Timer.innerHTML = `<h1>HAPPY INDEPENDANCE DAY!!!</h1>`
+
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
